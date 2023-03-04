7b17b18d64ff7e7969912f2aa547b8db9fffc56a
dominici94 <mattiadominici94@gmail.com>
2023-03-04 10:35:42 +0100

    add sprite file

--- portfolio/src/components/macro/Footer.vue
+++ portfolio/src/components/macro/Footer.vue
@@ -2,13 +2,26 @@
   <div class="contacts">
     <ul class="contacts__links">
       <li>
-        <a href="#">github icon</a>
+        <a href="#"
+          ><svg class="icon__mail">
+            <use xlink:href="../../assets/img/sprite.svg#icon-gmail"></use></svg
+        ></a>
       </li>
       <li>
-        <a href="#">linkedin icon</a>
+        <a href="#"
+          ><svg class="icon__linkedin">
+            <use
+              xlink:href="../../assets/img/sprite.svg#icon-linkedin"
+            ></use></svg
+        ></a>
       </li>
       <li>
-        <a href="#">gmail icon</a>
+        <a href="#"
+          ><svg class="icon__github">
+            <use
+              xlink:href="../../assets/img/sprite.svg#icon-github"
+            ></use></svg
+        ></a>
       </li>
     </ul>
 
