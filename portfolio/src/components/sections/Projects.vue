f8c12e435a3262d89acd2cae7a6a09d8a987c3ce
dominici94 <mattiadominici94@gmail.com>
2023-03-05 17:02:49 +0100

    all preview added

--- portfolio/src/components/sections/Projects.vue
+++ portfolio/src/components/sections/Projects.vue
@@ -23,36 +23,13 @@
             >
           </div>
         </li>
-        <!-- <a
-            class="projects__link"
-            href="https://boolflix-dominici.netlify.app"
-          >
-            <img :src="`${project.imgUrl}`" alt="preview image project" />
-          </a> -->
-        <!-- <li class="projects__item">
-          <a href="https://course-omnifood.netlify.app">Omnifood</a>
-        </li>
-        <li class="projects__item">
-          <a href="https://css-course-natours.netlify.app">Natours</a>
-        </li>
-        <li class="projects__item">
-          <a href="https://css-course-flexbox-trillo.netlify.app">Trillo</a>
-        </li>
-        <li class="projects__item">
-          <a href="https://css-course-grid-nexter.netlify.app">Nexter</a>
-        </li>
-        <li class="projects__item">
-          <a href="https://js-course-piggame.netlify.app">Pig game</a>
-        </li>
-        <li class="projects__item">
-          <a href="https://js-course-bankist.netlify.app">Bankist</a>
-        </li>
-        <li class="projects__item">
-          <a href="https://js-course-mapty.netlify.app">Mapty</a>
-        </li>
+        <!-- 
+        
+
         <li class="projects__item">
           <a href="https://js-course-forkify.netlify.app">Forkify</a>
-        </li> -->
+        </li> 
+        -->
       </ul>
     </div>
   </section>
@@ -73,51 +50,58 @@ export default {
         },
         {
           title: "Omnifood",
-          link: "https://boolflix-dominici.netlify.app",
-          desc: "Replica di Netflix, con funzionalità di ricerca di un film o serie tv tramite l'API di The Movie DB",
+          link: "https://course-omnifood.netlify.app",
+          desc: "prova",
           imgUrl: require("../../assets/img/preview_omnifood.png"),
           reponsive: false,
         },
         {
-          title: "Boolflix",
-          desc: "Replica di Netflix, con funzionalità di ricerca di un film o serie tv tramite l'API di The Movie DB",
-          imgUrl: require("../../assets/img/preview_boolflix.png"),
+          title: "Natours",
+          link: "https://css-course-natours.netlify.app",
+          desc: "prova prova",
+          imgUrl: require("../../assets/img/preview_natours.png"),
           reponsive: false,
         },
         {
-          title: "Boolflix",
-          desc: "Replica di Netflix, con funzionalità di ricerca di un film o serie tv tramite l'API di The Movie DB",
-          imgUrl: require("../../assets/img/preview_boolflix.png"),
+          title: "Trillo",
+          link: "https://css-course-flexbox-trillo.netlify.app",
+          desc: "provaprovaprvoa",
+          imgUrl: require("../../assets/img/preview_trillo.png"),
           reponsive: false,
         },
         {
-          title: "Boolflix",
-          desc: "Replica di Netflix, con funzionalità di ricerca di un film o serie tv tramite l'API di The Movie DB",
-          imgUrl: require("../../assets/img/preview_boolflix.png"),
+          title: "Nexter",
+          link: "https://css-course-grid-nexter.netlify.app",
+          desc: "Prova",
+          imgUrl: require("../../assets/img/preview_nexter.png"),
           reponsive: false,
         },
         {
-          title: "Boolflix",
+          title: "Pig game",
+          link: "https://js-course-piggame.netlify.app",
           desc: "Replica di Netflix, con funzionalità di ricerca di un film o serie tv tramite l'API di The Movie DB",
-          imgUrl: require("../../assets/img/preview_boolflix.png"),
+          imgUrl: require("../../assets/img/preview_piggame.png"),
           reponsive: false,
         },
         {
-          title: "Boolflix",
-          desc: "Replica di Netflix, con funzionalità di ricerca di un film o serie tv tramite l'API di The Movie DB",
-          imgUrl: require("../../assets/img/preview_boolflix.png"),
+          title: "Bankist",
+          link: "https://js-course-bankist.netlify.app",
+          desc: "Proiva",
+          imgUrl: require("../../assets/img/preview_bankist.png"),
           reponsive: false,
         },
         {
-          title: "Boolflix",
-          desc: "Replica di Netflix, con funzionalità di ricerca di un film o serie tv tramite l'API di The Movie DB",
-          imgUrl: require("../../assets/img/preview_boolflix.png"),
+          title: "Mapty",
+          link: "https://js-course-mapty.netlify.app",
+          desc: "prova prova",
+          imgUrl: require("../../assets/img/preview_mapty.png"),
           reponsive: false,
         },
         {
-          title: "Boolflix",
-          desc: "Replica di Netflix, con funzionalità di ricerca di un film o serie tv tramite l'API di The Movie DB",
-          imgUrl: require("../../assets/img/preview_boolflix.png"),
+          title: "Forkify",
+          link: "https://js-course-forkify.netlify.app",
+          desc: "provonma",
+          imgUrl: require("../../assets/img/preview_forkify.png"),
           reponsive: false,
         },
       ],
